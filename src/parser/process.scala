62f71673dfcaf39280289d317f486ff0485be58c
Elesthor <Elesthor@Gemini.local>
2014-01-31 12:15:18 +0100

    Start of a printer for the AST

--- src/parser/process.scala
+++ src/parser/process.scala
@@ -8,24 +8,14 @@
 //
 //
 
+class Channel // Temporary class to reprend outputs channels.
+
 abstract class Process
 
-class Channel
-
-// 0
-case class PTrivial()                                         extends Process
-// in(c, x).p
-case class PIn(c: Channel, x: TVar, p: Process)                  extends Process
-
-// in^k(c, x->u as y).p
-case class PInk(c:Channel, x: TVar, u: Term, y: TVar, p: Process) extends Process
-
-// out(c, t).p
-case class POut(c:Channel, t:Term, p:Process)                 extends Process
-
-// if v then pIf else pElse
-case class PIf(v:TValue, pIf:Process, pElse:Process)           extends Process
-
-// new s.p
-case class PNew(s:VConst, p:Process)                          extends Process
+case class PTrivial                                              extends Process
+case class PIn (c: Channel, x: TVar, p: Process)                 extends Process
+case class PInk(c:Channel, x: TVar, u: Term, y: TVar, p: Process)extends Process
+case class POut(c: Channel, t: Term, p: Process)                 extends Process
+case class PIf (v: TValue, pIf: Process, pElse: Process)         extends Process
+case class PNew(s: VConst, p: Process)                           extends Process
 
--- src/parser/terms.scala
+++ src/parser/terms.scala
@@ -17,18 +17,48 @@ abstract class Term
 class Value()                                       extends Term
 
 // Binding class between Term and value to have a case class.
-case class TValue (v: Value)                        extends Term
+case class TValue (v: Value)                        extends Term{
+    override def toString(): String = v.toString
+}
 
-case class VInt(v: Int)                             extends Value
-case class VCount(l: ListTerm)                      extends Value
-case class VSup(left: Value, right: Value)          extends Value
-case class VEqual(left: Term, right: Term)          extends Value
-case class VAnd(left: Value, right: Value)          extends Value
-case class VOr(left: Value, right: Value)           extends Value
-case class VNot(v: Value)                           extends Value
-case class VConst(s: String)                        extends Value
+case class VInt  (v: Int)                           extends Value{
+    override def toString(): String = v.toString+"\n"
+}
+case class VCount(l: ListTerm)                      extends Value{
+    override def toString(): String =
+        "Count("+l.toString+")\n"
+}
+case class VSup  (left: Value, right: Value)        extends Value{
+    override def toString(): String =
+        "Sup:\n"+left.toString+right.toString+"\n"
+}
+case class VEqual(left: Term, right: Term)          extends Value{
+    override def toString(): String =
+        "Equal:\n"+left.toString+right.toString+"\n"
+}
+case class VAnd  (left: Value, right: Value)        extends Value{
+    override def toString(): String =
+        "And:\n"+left.toString+right.toString+"\n"
+}
+case class VOr   (left: Value, right: Value)        extends Value{
+    override def toString(): String =
+        "Or:\n"+left.toString+right.toString+"\n"
+}
+case class VNot  (v: Value)                         extends Value{
+    override def toString(): String = "Not:\n" + v.toString
+}
+case class VConst(s: String)                        extends Value{
+    override def toString(): String = s
+}
 
+val a = new VInt(5)
+val b = new VInt(1)
 
+val c = new VSup(a,b)
+val d = new VEqual(a, c)
+
+val toto = new TValue(d)
+println(toto.toString)
 ////////////////////////////////////////////////////////////////////////////////
 //                               Lists                                        //
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,12 +70,11 @@ class ListTerm                                      extends Term{
 ////////////////////////////////////////////////////////////////////////////////
 //                               Terms                                        //
 ////////////////////////////////////////////////////////////////////////////////
-case class TVar(x: String)                          extends Term
+case class TVar (x: String)                         extends Term
 case class TPair(left: Term, right: Term)           extends Term
-case class TPi1(t: Term)                            extends Term
-case class TPi2(t: Term)                            extends Term
-case class TEnc(left: Term, right: Term)            extends Term
-case class TDec(left: Term, right: Term)            extends Term
-case class TPk(v: Value)                            extends Term
-case class TSk(v: Value)                            extends Term
-
+case class TPi1 (t: Term)                           extends Term
+case class TPi2 (t: Term)                           extends Term
+case class TEnc (left: Term, right: Term)           extends Term
+case class TDec (left: Term, right: Term)           extends Term
+case class TPk  (v: Value)                          extends Term
+case class TSk  (v: Value)                          extends Term
