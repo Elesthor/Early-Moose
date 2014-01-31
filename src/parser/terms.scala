28fcbd557b249cc96f9ad81f4c7f20eac3eb3265
Elesthor <Elesthor@Gemini.local>
2014-01-31 19:00:59 +0100

    Fix of the printer

--- src/parser/terms.scala
+++ src/parser/terms.scala
@@ -8,61 +8,63 @@
 //
 //
 
-abstract class Term
+abstract class Term{
+    def retString (x: Int): String
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                               Values                                       //
 ////////////////////////////////////////////////////////////////////////////////
 
-class Value()                                       extends Term
+abstract class Value extends Term{
+    def retString (x: Int): String
+}
 
 // Binding class between Term and value to have a case class.
-case class TValue (v: Value)                        extends Term{
-    override def toString(): String = v.toString
+case class TValue (v: Value) extends Term{
+     def retString(x: Int): String = " "*x+v.retString(x)
 }
 
-case class VInt  (v: Int)                           extends Value{
-    override def toString(): String = v.toString+"\n"
+case class VInt  (v: Int) extends Value{
+     def retString(x: Int): String = return " "*x+v.toString+"\n"
 }
-case class VCount(l: ListTerm)                      extends Value{
-    override def toString(): String =
-        "Count("+l.toString+")\n"
+
+// case class VCount(l: ListTerm)                      extends Value{
+//      def retString(x: Int): String =
+//        " "*x+"Count("+l.retString(x)+")\n"
+// }
+
+case class VSup  (left: Value, right: Value) extends Value{
+     def retString(x: Int): String =
+        " "*x+"Sup:\n"+left.retString(x+1)+right.retString(x+1)
 }
-case class VSup  (left: Value, right: Value)        extends Value{
-    override def toString(): String =
-        "Sup:\n"+left.toString+right.toString+"\n"
-}
+
 case class VEqual(left: Term, right: Term)          extends Value{
-    override def toString(): String =
-        "Equal:\n"+left.toString+right.toString+"\n"
-}
-case class VAnd  (left: Value, right: Value)        extends Value{
-    override def toString(): String =
-        "And:\n"+left.toString+right.toString+"\n"
-}
-case class VOr   (left: Value, right: Value)        extends Value{
-    override def toString(): String =
-        "Or:\n"+left.toString+right.toString+"\n"
-}
-case class VNot  (v: Value)                         extends Value{
-    override def toString(): String = "Not:\n" + v.toString
-}
-case class VConst(s: String)                        extends Value{
-    override def toString(): String = s
+     def retString(x: Int): String =
+        " "*x+"Equal:\n"+left.retString(x+1)+right.retString(x+1)
 }
 
-val a = new VInt(5)
-val b = new VInt(1)
+case class VAnd  (left: Value, right: Value) extends Value{
+     def retString(x: Int): String =
+        " "*x+"And:\n"+left.retString(x+1)+right.retString(x+1)
+}
+case class VOr   (left: Value, right: Value) extends Value{
+     def retString(x: Int): String =
+        " "*x+"Or:\n"+left.retString(x+1)+right.retString(x+1)
+}
+case class VNot  (v: Value) extends Value{
+     def retString(x: Int): String =
+        " "*x+"Not:\n" + v.retString(x+1)
+}
+case class VConst(s: String) extends Value{
+     def retString(x: Int): String = " "*x+s
+}
 
-val c = new VSup(a,b)
-val d = new VEqual(a, c)
 
-val toto = new TValue(d)
-println(toto.toString)
 ////////////////////////////////////////////////////////////////////////////////
 //                               Lists                                        //
 ////////////////////////////////////////////////////////////////////////////////
-class ListTerm                                      extends Term{
+abstract class ListTerm                                      extends Term{
     val content: Option[List[Term]] = None
 }
 
@@ -70,11 +72,53 @@ class ListTerm                                      extends Term{
 ////////////////////////////////////////////////////////////////////////////////
 //                               Terms                                        //
 ////////////////////////////////////////////////////////////////////////////////
-case class TVar (x: String)                         extends Term
-case class TPair(left: Term, right: Term)           extends Term
-case class TPi1 (t: Term)                           extends Term
-case class TPi2 (t: Term)                           extends Term
-case class TEnc (left: Term, right: Term)           extends Term
-case class TDec (left: Term, right: Term)           extends Term
-case class TPk  (v: Value)                          extends Term
-case class TSk  (v: Value)                          extends Term
+case class TVar (p: String) extends Term{
+    def retString(x: Int): String = " "*x+p
+}
+
+case class TPair(left: Term, right: Term) extends Term{
+    def retString(x: Int): String =
+        " "*x+"Pair:\n"+left.retString(x+1)+right.retString(x+1)
+}
+
+case class TPi1 (t: Term) extends Term{
+    def retString(x: Int): String =
+        " "*x+"Pi1:\n"+t.retString(x)+"\n"
+}
+
+case class TPi2 (t: Term) extends Term{
+    def retString(x: Int): String =
+        " "*x+"Pi2:\n"+t.retString(x)+"\n"
+}
+
+case class TEnc (left: Term, right: Term) extends Term{
+    def retString(x: Int): String =
+        " "*x+"Enc:\n"+left.retString(x+1)+right.retString(x+1)
+}
+case class TDec (left: Term, right: Term) extends Term{
+    def retString(x: Int): String =
+        " "*x+"Dec:\n"+left.retString(x+1)+right.retString(x+1)
+}
+
+case class TPk  (v: Value) extends Term{
+    def retString(x: Int): String = " "*x+"Pk:\n"+v.retString(x+1)+"\n"
+}
+
+case class TSk  (v: Value) extends Term{
+    def retString(x: Int): String = " "*x+"Sk:\n"+v.retString(x+1)+"\n"
+}
+
+
+
+val a = new VInt(5)
+val b = new VInt(1)
+val x = new VConst("x")
+
+val c = new VSup(a,b)
+val d = new VAnd(a, c)
+
+val e = new TSk(d)
+val f = new VEqual(e, a)
+
+println(f.retString(0))
+
