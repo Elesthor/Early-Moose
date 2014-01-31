03af8eabf000c50b3b6477d36fd766723b36402b
Olivier Marty <olivier.marty.m@gmail.com>
2014-01-31 11:13:55 +0100

    correction process

--- src/parser/process.scala
+++ src/parser/process.scala
@@ -3,7 +3,7 @@ abstract class Process
 class Channel
 
 // 0
-case class PTrivial extends Process
+case class PTrivial() extends Process
 
 // in(c, x).p
 case class PIn(c:Channel, x:TVar, p:Process) extends Process
@@ -15,7 +15,7 @@ case class PInk(c:Channel, x:TVar, u:Term, y:TVar, p:Process) extends Process
 case class POut(c:Channel, t:Term, p:Process) extends Process
 
 // if v then pIf else pElse
-case class PIf(v:TVal, pIf:Process, pElse:Process) extends Process
+case class PIf(v:Value, pIf:Process, pElse:Process) extends Process
 
 // new s.p
-case class PNew(s:TVal, p:Process) extends Process
+case class PNew(s:VConst, p:Process) extends Process
