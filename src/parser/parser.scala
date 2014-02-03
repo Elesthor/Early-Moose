9426e336a7b52f0a89e8d302c4c97de3a49a7214
Elesthor <Elesthor@Gemini.local>
2014-02-03 17:12:14 +0100

    Recurvise version of get word +inlining of Peek()

--- src/parser/parser.scala
+++ src/parser/parser.scala
@@ -14,53 +14,43 @@ abstract class Input
 {
   type Ch = Char
   def GetChar() : Ch
-  
+
   var line = 0
   var col = 0
   var peeked : Option[Ch] = None
   def Peek(expected: Ch => Boolean) : Ch =
   {
-    val c = Peek();
-    if(expected(c))
-      c
-    else
-      throw new Unexpected(c, expected);
-  }
-  def Peek() =
-  {
     val c = GetChar()
     peeked = Some(c)
-    c
+    if(expected(c)) c else throw new Unexpected(c, expected);
   }
+
+
   def GetPeeked () =
   {
     val c = peeked.get // throw an exception if peeked is None
     peeked = None
     c
   }
+
   def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) : String =
   {
-    def allExpected = { x : Ch => expected(x) || delimiters(x) }
-    var word = ""
-    var ok = true
-    while(ok)
-    {
-      val c = Peek(allExpected)
-      if(expected(c))
-        word += c
-      else
-        ok = false
+    def collector(): String = {
+      val c = Peek({ x : Ch => expected(x) || delimiters(x) })
+      if (expected(c)) c+collector() else ""
     }
-    word
+    collector()
   }
+
+
   def GetNumber() =
   {
     Integer.parseInt(GetWord(numeric, {x => true}))
   }
-  
+
   case class Unexpected(c:Ch, expected: Ch => Boolean) extends Exception
   case class EndOfFile() extends Exception
-  
+
   def numeric  (x : Ch) = { x >= '0' && x <= '9' }
   def alphaLow (x : Ch) = { x >= 'a' && x <= 'z' }
   def alphaUp  (x : Ch) = { x >= 'A' && x <= 'Z' }
@@ -73,7 +63,7 @@ import java.io.FileInputStream
 class InputFromFile(file:String) extends Input
 {
   val inStream = new FileInputStream(file)
-  
+
   def GetChar() =
   {
     val c = inStream.read()
@@ -86,7 +76,7 @@ class InputFromFile(file:String) extends Input
       col = col + 1
     if(c== -1)
       throw EndOfFile()
-    
+
     Character.toChars(c)(0) // convert Int to Char
   }
 }
@@ -94,14 +84,30 @@ class InputFromFile(file:String) extends Input
 class Parser(src : Input)
 {
   case class SyntaxError(line:Int, col:Int) extends Exception
-  
-  def ParseProcess() =
+
+  def CheckPeeked(c : Char)
+  {
+    if(src.GetPeeked() != c)
+      throw SyntaxError(src.line, src.col)
+  }
+
+  def ParseProcess() : Process =
   {
     val keyword = src.GetWord(src.alpha, {x : Char => src.parenthesis(x) || x == ' ' || x == '^' || x == '0'})
     val peeked = src.GetPeeked()
     (keyword, peeked) match
     {
-      case ("in", '(') => new PTrivial
+      case ("in", '(') =>
+        val channel = new Channel(src.GetNumber())
+        CheckPeeked(',')
+
+        val variable = new TVar(src.GetWord(src.alpha, { x : Char => x == ')' }))
+        // no need to check if peeked = Some(')')
+        src.Peek()
+        CheckPeeked('.')
+
+        new PIn(channel, variable, ParseProcess())
+
       case ("in", '^') => new PTrivial
       case ("out", '(') => new PTrivial
       case ("if", ' ') => new PTrivial
@@ -112,8 +118,12 @@ class Parser(src : Input)
   }
 }
 
-val in = new InputFromFile("test")
-val p = new Parser(in)
-p.ParseProcess()
-
+object TestParser
+{
+  def main(args: Array[String])
+  {
+    val p = new Parser(new InputFromFile("test"))
+    //p.ParseProcess().retString()
+  }
+}
 
