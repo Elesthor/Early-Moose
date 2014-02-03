91b7e6734bac047bc18ce5eba569394fea8bc2ba
Olivier Marty <olivier.marty.m@gmail.com>
2014-02-03 15:14:58 +0100

    Reorganization of Input and InputFromFile

--- src/parser/parser.scala
+++ src/parser/parser.scala
@@ -10,37 +10,71 @@
 
 
 
-trait Input
+trait Input[Ch]
 {
-  type Ch
   def GetChar() : Ch
-  def GetChar(expected: Ch => Boolean) : Ch
-  def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) : String
+  def numeric(x : Ch) : Boolean
+  def alphaLow(x : Ch) : Boolean
+  def alphaUp(x : Ch) : Boolean
+  
+  var line = 0
+  var col = 0
+  var peeked : Option[Ch] = None
+  def Peek(expected: Ch => Boolean) : Ch =
+  {
+    val c = Peek();
+    if(expected(c))
+      c
+    else
+      throw new Unexpected(c, expected);
+  }
+  def Peek() =
+  {
+    val c = GetChar()
+    peeked = Some(c)
+    c
+  }
+  def GetPeeked () =
+  {
+    val c = peeked.get // throw an exception if peeked is None
+    peeked = None
+    c
+  }
+  def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) : String =
+  {
+    def allExpected = { x : Ch => expected(x) || delimiters(x) }
+    var word = ""
+    var ok = true
+    while(ok)
+    {
+      val c = Peek(allExpected)
+      if(expected(c))
+        word += c
+      else
+        ok = false
+    }
+    word
+  }
   def GetNumber() =
   {
     Integer.parseInt(GetWord(numeric, {x => true}))
   }
-  var line : Int
-  var col : Int
+  
   case class Unexpected(c:Ch, expected: Ch => Boolean) extends Exception
-  def numeric(x : Ch) : Boolean
-  def alphaLow(x : Ch) : Boolean
-  def alphaUp(x : Ch) : Boolean
+  
   def alpha(x : Ch) = { alphaLow(x) || alphaUp(x) }
   def alphaNumeric(x : Ch) = { alpha(x) || numeric(x) }
   def parenthesis(x : Ch) = { x == '(' || x == ')' }
 }
 
 import java.io.FileInputStream
-class InputFromFile(file:String) extends Input
+class InputFromFile(file:String) extends Input[Int]
 {
-  type Ch = Int
-  val inStream = new FileInputStream(file) // TODO : le fermer dans un destructeur
-  var line = 0
-  var col = 0
-  def numeric  (x : Ch) = { x >= '0' && x <= '9' }
-  def alphaLow (x : Ch) = { x >= 'a' && x <= 'z' }
-  def alphaUp  (x : Ch) = { x >= 'A' && x <= 'Z' }
+  val inStream = new FileInputStream(file)
+  
+  def numeric  (x : Int) = { x >= '0' && x <= '9' }
+  def alphaLow (x : Int) = { x >= 'a' && x <= 'z' }
+  def alphaUp  (x : Int) = { x >= 'A' && x <= 'Z' }
   
   def GetChar() =
   {
@@ -54,35 +88,9 @@ class InputFromFile(file:String) extends Input
       col = col + 1
     c
   }
-  
-  def GetChar(expected: Ch => Boolean) =
-  {
-    val c = GetChar();
-    if(expected(c))
-      c
-    else
-      throw new Unexpected(c, expected);
-  }
-  
-  def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) =
-  {
-    var c = 0
-    var word = ""
-    def allExpected = {x : Ch => expected(x) || delimiters(x)}
-    var ok = true
-    while(ok)
-    {
-      c = GetChar(allExpected)
-      if(expected(c))
-        word += c
-      else
-        ok = false
-    }
-    word
-  }
 }
 
-class parser(src : Input)
+class parser(src : Input[Int])
 {
   def ParseProcess =
   {
