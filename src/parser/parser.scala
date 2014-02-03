af6575a345516109b13033b2165ace6903b0e421
Olivier Marty <olivier.marty.m@gmail.com>
2014-02-03 13:51:30 +0100

    les classes de caractères sont gérées par des fonctions booléennes

--- src/parser/parser.scala
+++ src/parser/parser.scala
@@ -12,20 +12,40 @@
 
 trait Input
 {
-  def GetChar() : Int
-  def GetChar(expected:Array[Int]) : Int
-  def GetWord(expected:Array[Int], delimiters:Array[Int]) : String
+  type Ch
+  def GetChar() : Ch
+  def GetChar(expected: Ch => Boolean) : Ch
+  def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) : String
   var line : Int
   var col : Int
-  case class Unexpected(c:Int, expected:Array[Int]) extends Exception
+  case class Unexpected(c:Ch, expected: Ch => Boolean) extends Exception
+  def numeric(x : Ch) : Boolean
+  def alphaLow(x : Ch) : Boolean
+  def alphaUp(x : Ch) : Boolean
+  def alpha(x : Ch) = { alphaLow(x) || alphaUp(x) }
+  def alphaNumeric(x : Ch) = { alpha(x) || numeric(x) }
+  def parenthesis(x : Ch) = { x == '(' || x == ')' }
 }
 
 import java.io.FileInputStream
 class FromFile(file:String) extends Input
 {
+  type Ch = Int
   val inStream = new FileInputStream(file)
   var line = 0
   var col = 0
+  def numeric (x : Ch) =
+  {
+    x >= '0' && x <= '9'
+  }
+  def alphaLow (x : Ch) =
+  {
+    x >= '0' && x <= '9'
+  }
+  def alphaUp (x : Ch) =
+  {
+    x >= '0' && x <= '9'
+  }
   
   def GetChar() =
   {
@@ -37,29 +57,28 @@ class FromFile(file:String) extends Input
     }
     else
       col = col + 1
-    
     c
   }
   
-  def GetChar(expected:Array[Int]) =
+  def GetChar(expected: Ch => Boolean) =
   {
     val c = GetChar();
-    if(expected.contains(c))
+    if(expected(c))
       c
     else
       throw new Unexpected(c, expected);
   }
   
-  def GetWord(expected:Array[Int], delimiters:Array[Int]) =
+  def GetWord(expected: Ch => Boolean, delimiters: Ch => Boolean) =
   {
     var c = 0
     var word = ""
-    val allExpected = expected++delimiters
+    def allExpected = {x : Ch => expected(x) || delimiters(x)}
     var ok = true
     while(ok)
     {
       c = GetChar(allExpected)
-      if(expected.contains(c))
+      if(expected(c))
         word += c
       else
         ok = false
@@ -75,4 +94,5 @@ class parser(src : Input)
     //var name = src.GetWord();
   }
 }
+
 
