24a937c9d5a186f5db103b416cffa531912caed4
Olivier Marty <olivier.marty.m@gmail.com>
2014-02-27 20:06:25 +0100

    exception file not found

--- src/main.scala
+++ src/main.scala
@@ -8,7 +8,6 @@
 //
 //
 
-
 object Application
 {
   def main(args: Array[String]): Unit =
@@ -20,27 +19,32 @@ object Application
     }
     else
     {
-      val t = new Interpretor(args(0)=="-sync")
-      
-      // parsing
-      val src = new InputFromFile(args(1))
-      val p = new Parser(src)
       try
       {
-        val program = p.Parse()
-        println("end of parsing")
-        
-        // playing
-        (new Interpretor(args(0) == "-sync")).InterpretMetaProc(program)
+        val src = new InputFromFile(args(1))
+        val p = new Parser(src)
+        try
+        {
+          // parsing
+          val program = p.Parse()
+          println("end of parsing")
+          
+          // playing
+          (new Interpretor(args(0) == "-sync")).InterpretMetaProc(program)
+        }
+        catch
+        {
+          case p.SyntaxError()       => System.err.println("Syntax Error (line " + src.line + "; col " + src.col + ")\n")
+          case p.ValueExpected()     => System.err.println("A value was expected (line " + src.line + "; col " + src.col + ")\n")
+          case p.NameMalformed(name) => System.err.println("Malformed name : '" + name + "' (line " + src.line + "; col " + src.col + ")\n")
+          case src.EndOfFile()       => System.err.println("End of file unexpected (line " + src.line + "; col " + src.col + ")\n")
+          case src.Unexpected(c, f)  =>
+            System.err.println("Character '" + src.CharToString(c) + "' unexpected (line " + src.line + "; col " + src.col + ")\nExpected : " + f.serialized)
+        }
       }
       catch
       {
-        case p.SyntaxError()       => println("Syntax Error (line " + src.line + "; col " + src.col + ")\n")
-        case p.ValueExpected()     => println("A value was expected (line " + src.line + "; col " + src.col + ")\n")
-        case p.NameMalformed(name) => println("Malformed name : '" + name + "' (line " + src.line + "; col " + src.col + ")\n")
-        case src.EndOfFile()       => println("End of file unexpected (line " + src.line + "; col " + src.col + ")\n")
-        case src.Unexpected(c, f)  =>
-          println("Character '" + src.CharToString(c) + "' unexpected (line " + src.line + "; col " + src.col + ")\nExpected : " + f.serialized)
+        case _:java.io.FileNotFoundException => System.err.println("file " + args(1) + " not found")
       }
     }
   }
