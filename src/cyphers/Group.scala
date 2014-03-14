17a335faaa15b685394cf64b1ab035cbc14e9780
Olivier Marty <olivier.marty.m@gmail.com>
2014-03-14 19:50:41 +0100

    file Group.scala with some cyclic groups

---
+++ src/cyphers/Group.scala
@@ -0,0 +1,39 @@
+
+trait Group[E]
+{
+  val unit: E
+  val generator: E
+  def times (e1: E, e2: E) : E
+  def exp (e: E, n: Int) : E =
+  {
+    if(n % 2 == 0)
+      exp(times(e, e), n/2)
+    else if(n == 1)
+      e
+    else
+      times(exp(times(e, e), n/2), e)
+  }
+}
+
+class Zk(k: Int) extends Group[Int]
+{
+  val unit = 0
+  val generator = 1
+  def times (e1: Int, e2: Int) : Int =
+  {
+    (e1+e2)%k
+  }
+}
+
+class Zp(p: Int) extends Group[Int]
+{
+  val unit = 1
+  val generator = 2
+  def times (e1: Int, e2: Int) : Int =
+  {
+    (e1*e2)%p
+  }
+}
+
+// TODO : courbe elliptique
+
