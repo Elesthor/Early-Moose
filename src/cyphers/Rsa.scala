1daf5602d7b8bec4b2ab3d5882fb2188723d483a
Elestho <Elesthor@Gemini.local>
2014-03-19 16:09:51 +0100

    bug fixes on RSA

--- src/cyphers/Rsa.scala
+++ src/cyphers/Rsa.scala
@@ -21,6 +21,8 @@
 
 import scala.BigInt
 
+val PQ_LENGTH = 1024
+
 abstract class Key [T]
 {
   def getPublic(): T
@@ -38,8 +40,8 @@ class RsaKey (seed: Int) extends Key [(BigInt, BigInt)]
   def generate() =
   {
     val randomizer = new util.Random(seed)
-    val p = BigInt.apply(1024, 100, randomizer)
-    val q = BigInt.apply(1024, 100, randomizer)
+    val p = BigInt.apply(PQ_LENGTH, 100, randomizer)
+    val q = BigInt.apply(PQ_LENGTH, 100, randomizer)
     val n = p*q
     val indEulerN = (p-1)*(q-1)
     
@@ -60,41 +62,44 @@ class RsaKey (seed: Int) extends Key [(BigInt, BigInt)]
 
 class RSA extends CryptoSystem [(BigInt, BigInt)]
 {
-  def asciiRepr(s: Byte): String = 
+  // Return a padded representation of the byte at len digit.
+  def padByte(s: String , len: Int): String = 
   {
-   "0"*(3-s.toString.length)+s.toString
+    "0"*(len-s.toString.length)+s.toString
   }
-  def PKCS1StringToInt(msg: String): BigInt = 
+
+  def PKCS1StringToInt(msg: String): String  = 
   {
-    BigInt.apply(msg.getBytes.map(asciiRepr).foldLeft(""){(s,c)=>s+c})
+    (msg.getBytes.map({x => padByte(x.toString, 3)})).foldLeft(""){(s,c)=>s+c}
   }
 
   def PKCS1IntToString(nb: BigInt): String = 
   {
-    nb.toString.grouped(3).toArray.map(_.toInt.toChar).foldLeft(""){(s,c)=>s+c}
+    (nb.toString.grouped(3).toArray.map(_.toInt.toChar).foldLeft(""){(s,c)=>s+c})
   }
 
   def encrypt(msg: String, key: Key[(BigInt, BigInt)]): String = 
   {
     val (n, e) = key.getPublic
-    (PKCS1StringToInt(msg).modPow(e,n)).toString
+    val chunks = PKCS1StringToInt(msg).grouped(PQ_LENGTH).toArray  
+    chunks.map({x => padByte(BigInt.apply(x).modPow(e,n).toString, PQ_LENGTH)}).foldLeft(""){(s,c)=>s+c}
   }
 
   def decrypt(crypt: String, key: Key[(BigInt, BigInt)] ): String = 
   {
     val (n, d) = key.getPrivate
-    PKCS1IntToString(BigInt.apply(crypt).modPow(d,n))
+    val chunks = crypt.grouped(PQ_LENGTH).toArray 
+
+    val decrypted =  chunks.map(BigInt.apply(_).modPow(d,n).toString).foldLeft(""){(s,c)=>s+c}
+    PKCS1IntToString(BigInt.apply(decrypted))
   }
 
 }
 
 val a = new RsaKey(12)
-val b = "tot boubou , salut, foo bar !! , coucou"
+val b = "uhuhuhuhuhuhuhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnuhuhuhuhuhuhuhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5uhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5uhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5uhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5uhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5uhuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjjjjjjjjjjjjjjjjjjbbbbbbbbbbbbbbbbbbbbbbjjjjjjjjjjtestcoucou12345123451234512345123451234512345123451234512345123451234bcdefghijklmnop5"
 val rsa = new RSA
-val c = rsa.PKCS1StringToInt(b)
-println(c)
-val d = rsa.PKCS1IntToString(c)
-println(d)
+
 val f = rsa.encrypt(b, a)
 println(f)
 println(rsa.decrypt(f, a))
