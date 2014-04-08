90bd711502c07399e318983a8e0eb632ac721884
Elesthor <elesthor@gmail.com>
2014-04-08 18:46:22 +0200

    fix for typing in main.scala

--- src/cyphers/main.scala
+++ src/cyphers/main.scala
@@ -20,7 +20,7 @@ object Application
   def main(args: Array[String]): Unit =
   {
     // option ligne de commande
-    if(args.length < 3 || (args(0) != "--RSA" && 
+    if(args.length < 4 || (args(0) != "--RSA" && 
                            args(0) != "--Vigenere"
                            args(0) != "--Cesar"
                            args(0) != "--Enigma"
@@ -36,65 +36,65 @@ object Application
         case "--RSA"      => 
         { 
           val cypher = new RSA()
-          val key = new RsaKey(args(2))
+          val key = new RsaKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
 
         case "--Vigenere" =>
         { 
           val cypher = new Vigenere()
-          val key = new VigenereKey(args(2))
+          val key = new VigenereKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
       
         case "--Cesar"    =>
         {
           val cypher = new Cesar()
-          val key = new CesarKey(args(2))
+          val key = new CesarKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
         case "--Enigma"   =>
         {
           val cypher = new Cesar()
-          val key = new EnigmaKey(args(2))
+          val key = new EnigmaKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
 
         case"--Elgamal"   =>
           {
           val cypher = new ElGamal()
-          val key = new ElGamalKey(args(2))
+          val key = new ElGamalKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
         case "--AES"      =>
         {
           val cypher = new AES()
-          val key = new AESKey(args(2))
+          val key = new AESKey(args(2).toString)
           if (args(1) = "encrypt")
           {
-            cypher.encrypt(args(3), key, args(2))
+            cypher.encrypt(args(3), key, args(2).toString)
           }
-          else cypher.encrypt(args(3), key, args(2))
+          else cypher.encrypt(args(3), key, args(2).toString)
         }
       }
     }
