dafaaa6df52884094ab3301bdae456acd6fdd749
Elesthor <elesthor@gmail.com>
2014-05-18 09:25:24 +0200

    improvement on cesarand vigenere opponenent

--- src/cyphers/Cesar.scala
+++ src/cyphers/Cesar.scala
@@ -56,23 +56,35 @@ class EncapsulatedCesar extends EncapsulatedCrypto
 class CesarOpponent 
 {
   val FREQ_FR = scala.collection.immutable.Map( 
-      ('A',9.2),('B',1.02),('C',2.64),('D',3.39),('E',15.87),('F',0.95),('G',1.04),
-      ('H',0.77),('I',8.41), ('J',0.89),('K',0.00),('L',5.34),('M',3.24),('N',7.15),
-      ('O',5.14),('P',2.86), ('Q',1.06),('R',6.46),('S',7.90),('T',7.26),('U',6.24),
-      ('V',2.15),('W',0.00), ('X',0.30),('Y',0.24),('Z',0.32))
+      ('!',0.0427497311748),('%',0.000546763515274),(')',1.37707006828),
+      ('(',1.37701272883), ('+',6.09756097561e-05), ('-',0.0699495741795),
+      (',',1.67779290886), ('/',0.000569363676566),('.',0.369103254673),
+      ('1',6.84596406525),('0',6.1201961431),('3',6.69875001885),
+      ('2',6.62342654969),('5',6.76489186998),('4',6.64340751543),
+      ('7',6.71723783619), ('6',6.50060647488),('9',6.5606388158),
+      ('8',6.59759144067),(';',0.00999405387986),(':',3.01142446203),
+      ('?',0.0649862964664),('A',2.92366108479),('C',0.370234077642),
+      ('B',0.355911821756),('E',2.2405857491),('D',0.832105634308),
+      ('G',0.475451798364), ('F',0.37482563323),('I',2.79232391368),
+      ('H',1.21080206944),('K',0.147350957831),('J',0.0594822133613),
+      ('M',0.442836438055), ('L',0.712119615578),('O',1.68523284479),
+      ('N',1.16642172444),('Q',0.000551000752754),('P',1.62920836561),
+      ('S',1.11592692364), ('R',2.25477591137),('U',0.528778101758),
+      ('T',1.90436916684),('W',0.536821827016),('V',0.180590633963),
+      ('Y',0.476629186144),('X', 0.0), ('Z', 0.0),('[',0.384171689195),(']',0.384132976566),
+      ('<', 0.0), ('=', 0.0), ('>', 0.0), ('@', 0.0))
 
-  
   // Compute the frequency table of the letters occurence in the string inpt.
   def letterFrequencies(inpt: String) =
     inpt groupBy (c => c) mapValues (_.length.toFloat/inpt.length*100)
 
   // Filter the string str from all non-printable ascii characters.
   def filterString(str: String) = 
-    str.filter {x=> (x > 30  && x < 126) || x == 32 }
+    str.filter {x=> (x > 47  && x < 92) || (96 < x && x < 123) || List(32, 40, 41, 44, 46, 58, 93 ).contains(x) }
 
   def getLetters(str: String) = 
   {
-   ( (str.toUpperCase).filter { x => (x>64 && x<91) }  )
+   ( (str.toUpperCase).filter {x=> (x > 47  && x < 92) || List(40, 41, 44, 46, 58, 93 ).contains(x) }  )
   }
   
   
@@ -80,7 +92,7 @@ class CesarOpponent
   {
     var sum = 0.;
     var baseFreq = letterFrequencies(str);
-    for (i <- 65 to 90)
+    for (i <- (48 to 91).toList ++  List(40, 41, 44, 46, 58, 93 ))
     {
       val bqp = 
       (
@@ -89,9 +101,10 @@ class CesarOpponent
       )
       sum +=  (bqp - FREQ_FR(i.toChar))*(bqp - FREQ_FR(i.toChar))
     }
+
+
     sum
   }
-
 
   def openEnc(crypt: String) =
   {
@@ -105,6 +118,7 @@ class CesarOpponent
       val currentDecrypt = cypher.decrypt(crypt, currentKey);
       if (currentDecrypt == filterString(currentDecrypt))
       {
+
         val currentDist =  computeDist(getLetters(currentDecrypt))
         if (minimalValue > currentDist)
         {
--- src/cyphers/Vigenere.scala
+++ src/cyphers/Vigenere.scala
@@ -26,7 +26,7 @@ class VigenereKey(seed: Int) extends Key[Array[Byte]]
     // random Array[Byte] of random size
     val randomizer = new util.Random(seed)
     // nextString return string of any character (chinese...), we cast them to bytes
-    randomizer.nextString(randomizer.nextInt() % 64 + 64).toCharArray.map(_.toByte)
+    randomizer.nextString((randomizer.nextInt() % 8) +6).toCharArray.map(_.toByte)
   }
   
   def getPublic  = generate()
@@ -49,31 +49,6 @@ class Vigenere extends CryptoSystem [Array[Byte]]
     _encrypt (msg, key)
 }
 
-class VigenereOpponent
-{
-  def indexOfCoincidence (inpt: String) = 
-    ((inpt groupBy (c => c) mapValues (_.length.toFloat)).values.foldLeft (0.)
-      {(s,c)=> s*(s-1)+c}) / (inpt.length*(inpt.length-1)/26)
-
-  def filterString(str: String) = 
-    str.filter {x=> (x > 30  && x < 126) || x == 32 }
-
-  def getKeyLength (crypt: String) = 
-  {
-    def computeMoyIndex (s: String, k: Int) = 
-    {
-      def splitAt(x: Int) = 
-      {
-        var tmp = ""
-        for (i<- 0 to s.length-1 if i%k == x) tmp += s(i)
-        tmp
-      }
-      ( ((0 to k-1) map (x => indexOfCoincidence 
-                            (splitAt (x)))).foldLeft (0.) {(s,c) => s+c} ) / k
-    }
-    ((0 to crypt.length/2) map (x => (computeMoyIndex(crypt, x), x)) min)._2
-  }
-}
 
 
 class EncapsulatedVigenere extends EncapsulatedCrypto
@@ -85,4 +60,116 @@ class EncapsulatedVigenere extends EncapsulatedCrypto
   def decrypt(msg: String, key: Key[T]) = crypto.decrypt(msg, key)
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                            Vigenere Opponent                               //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+class VigenereOpponent
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//                      String manipulation Toolbox                           //
+////////////////////////////////////////////////////////////////////////////////
+
+
+  // Compute the index of coincidence of the inpt string.
+  def indexOfCoincidence (inpt: String) = 
+    ((inpt groupBy (c => c) mapValues (_.length.toFloat)).values.foldLeft (0.)
+      {(s,c)=> c*(c-1)+s}) / (inpt.length*(inpt.length-1))
+
+  
+  // Remove all non printable characters from the str string.
+  def filterString(str: String) = 
+    str.filter {x=> (x > 47  && x < 92) || (96 < x && x < 123) || List(32, 40, 41, 44, 46, 58, 93 ).contains(x) }
+
+  // Compute the substring constituted from the chars at positions which are 
+  // congrous to x mod k.
+  def splitAt(s: String, x: Int, k: Int) = 
+  {
+    var tmp = ""
+    for (i<- 0 to s.length-1 if i%k == x) tmp += s(i)
+    tmp
+  }
+
+
+  // Merge a list of list (generalisation of the zip function)
+  def combineLists[A](ss:List[A]*) = 
+    (ss.head.map(List(_)) /: ss.tail)(_.zip(_).map(p=>p._2 :: p._1))
+
+
+  // Zip a list of strings 
+  def recombine(l: List[String]) =
+  {
+    val maxLength = l.map(_.length).max;
+    val completedList = l.map(s => s+" "*(maxLength-s.length))
+    combineLists((completedList.map ( s => s.reverse.toList )):_*).map (_.mkString).foldLeft ("")((s,c)=>s+c).reverse
+  }
+
+////////////////////////////////////////////////////////////////////////////////
+//                        Cryptanalysis functions                             //
+////////////////////////////////////////////////////////////////////////////////
+  
+
+  // Compute the most probable key length, according to a given index of 
+  // coincidence for the language.
+  def getKeyLength (crypt: String) = 
+  {
+    def computeMoyIndex (s: String, k: Int) = 
+    {
+    (((0 to k-1) map (x => indexOfCoincidence 
+      (splitAt (s, x, k)))).foldLeft (0.) {(s,c) => s+c} ) / k
+    }
+    (((1 to 3) map 
+      (x => (scala.math.abs(computeMoyIndex(crypt, x)-0.0662), x))) min)._2
+  }
+  
+
+  // Return the closest cesar decryption for a given string, for the 
+  // euclidian distance of the frequencies table for the language.
+  def getClosestDec(crypt: String) =
+  {
+    val cypher = new Cesar()
+    val currentKey = new CesarKey(0) 
+    val opp = new CesarOpponent();
+    (0 to 255).map (i => {currentKey.setKey(i);
+    cypher.decrypt(crypt, currentKey)}).filter(x => x==filterString(x)).map(x=>(opp.computeDist(opp.getLetters(x)), x)).min._2
+      //.sortWith((x,y) => x._1 <= y._1)
+  }
+
+  def openEnc (crypt: String, k: Int) = 
+  {
+    //val keyLength = getKeyLength(crypt);
+    val keyLength = k;
+    val splited = ((0 to keyLength-1) map 
+        (x =>  (splitAt (crypt, x, keyLength))))
+    recombine(splited.map(x=>getClosestDec(x)).toList)
+
+  }
+}
+val c = new CesarOpponent()
+val cypher = new Vigenere();
+val opp = new VigenereOpponent();
+val toto = "[(1234,111098765432467890pai897654pair37689987656438903, pair)]"
+val key = new VigenereKey(5);
+println(key.getPublic.length)
+val crypt = cypher.encrypt(toto, key)
+println(crypt);
+println(opp.indexOfCoincidence(toto))
+println(opp.getKeyLength(crypt))
+for (i<- 1 to 13)
+{
+  try
+  {
+  val sp = opp.openEnc(crypt,i)
+  println(i+sp+"   "+ c.computeDist(sp), c.computeDist(toto))
+  
+  }
+  catch
+  {
+    case e: Exception => println("coucou")
+  }
+}
 
